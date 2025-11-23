7f6e1833f929c48873189ced8d22aadc17a39eb1
Meta-Dusk <andredcxxv@gmail.com>
2025-11-23 20:21:37 +0800

    Added an entity class and improved image class

--- src/entities/entity.py
+++ src/entities/entity.py
@@ -1,6 +1,44 @@
+import asyncio
+import flet as ft
+from images import Sprite
+from dataclasses import dataclass
+
 # TODO: Make base entity class that handles simple sprite and states
 
+@dataclass
+class EntityStates:
+    """Entity states and tasks."""
+    is_moving: bool = False
+    sprint: bool = False
+    jumped: bool = False
+    jump_task: asyncio.Task = None
+    attack_phase: int = 0
+    attack_task: asyncio.Task = None
+    is_attacking: bool = False
+    is_falling: bool = False
+
+@dataclass
+class EntityStats:
+    """Includes health, movement speed, etc."""
+    health: float = 20
+    movement_speed: int = 10
+    attack_damage: float = 5
+    attack_speed: float = 2
+
 class Entity:
-    def __init__(self):
-        pass
+    """Entity base class. Handles the sprite and some states."""
+    def __init__(self, sprite: Sprite, name: str):
+        self.sprite = sprite
+        self.name = name
+        self.states = EntityStates()
+        self.stats = EntityStats()
+        self.dx: int = 0
+        self.dy: int = 0
+        self.stack: ft.Stack = self._make_stack()
     
+    def _make_stack(self):
+        return ft.Stack(
+            controls=[self.sprite], left=0, bottom=0,
+            animate_position=ft.Animation(100, ft.AnimationCurve.EASE_IN_OUT)
+        )
+        
--- src/images.py
+++ src/images.py
@@ -18,16 +18,13 @@ class Sprite(ft.Image):
     def change_src(self, new_src: str, update_ctrl: bool = True):
         """Swap the `src` and optionally update."""
         self.src = new_src
-        if update_ctrl and self.page:
-            self.update()
+        if update_ctrl and self.page: self.update()
     
     def flip_x(self, update_ctrl: bool = True):
         """Flip the image on the x-axis."""
-        if (
-            self.scale.scale_x > 0 or self.scale.scale_x < 0
-        ): self.scale.scale_x *= -1
-        if update_ctrl and self.page:
-            self.update()
+        scale_x = self.scale.scale_x
+        if scale_x > 0 or scale_x < 0: self.scale.scale_x *= -1
+        if update_ctrl and self.page: self.update()
             
             
 # * Test for the Sprite class
@@ -36,9 +33,19 @@ def test(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
-    spr = Sprite("images/enemies/goblin/attack-main_0.png", 150, 150)
+    src_count: int = 0
+    
+    def update_src(_):
+        nonlocal src_count
+        if src_count < 7: src_count += 1
+        else: src_count = 0
+        spr.change_src(f"images/enemies/goblin/attack-main_{src_count}.png")
+    
+    spr = Sprite(f"images/enemies/goblin/attack-main_{src_count}.png", 150, 150)
     page.add(
-        spr, ft.Button("Flip (x-axis)", on_click=lambda _: spr.flip_x())
+        spr,
+        ft.Button("Flip (x-axis)", on_click=lambda _: spr.flip_x()),
+        ft.Button("Update Source", on_click=update_src)
     )
     
 if __name__ == "__main__":
